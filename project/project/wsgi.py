d5b41a01b110555116fad965021acae7a6f4bcf8
karthik03022004 <karthiksunny222@gmail.com>
2025-11-01 00:01:33 +0530

    env in wsgi,settings

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE', 'project.settings'))
+
+
 
 application = get_wsgi_application()
