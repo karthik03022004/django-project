85caf143f009989f3e7ea157a0cd2f83e6e2d821
karthik03022004 <karthiksunny222@gmail.com>
2025-11-01 10:11:14 +0530

    updated seetings

--- project/project/settings.py
+++ project/project/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
 env=environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
 # Quick-start development settings - unsuitable for production
